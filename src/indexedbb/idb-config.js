c1d1bae244462b70edfddb6f9f96bb5b3ecef575
Laurent Caouissin <laurent.caouissin@insee.fr>
2021-08-11 11:58:47 +0200

    Manage database version

--- src/indexedbb/idb-config.js
+++ src/indexedbb/idb-config.js
@@ -1,10 +1,13 @@
 import Dexie from 'dexie';
 import schema from './schema.json';
+import schema2 from './schema-2.json';
 
 export class LocalDbFactory extends Dexie {
   constructor(dataBaseName) {
     super(dataBaseName);
     this.version(1).stores(schema);
+    // upgrade dataBase (please see https://dexie.org/docs/Tutorial/Design#database-versioning)
+    this.version(2).stores(schema2);
   }
 
   getStore(name) {
