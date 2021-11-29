d9b2175b2a4e9a76b57db867c87bab9a8895a49c
Dylan Decrulle <dylan.decrulle@insee.fr>
2021-11-29 18:46:07 +0100

    Update component.js
    delete background color on queen component

--- src/components/panel-body/queen-container/component.js
+++ src/components/panel-body/queen-container/component.js
@@ -1,26 +1,14 @@
-import { makeStyles } from '@material-ui/core';
 import listenQueen from 'utils/hooks/listenQueen';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
-const useStyles = makeStyles(theme => ({
-  queenApp: {
-    backgroundColor: theme.palette.primary.main,
-  },
-}));
-
 const QueenContainer = queenSwState => {
   const history = useHistory();
-  const classes = useStyles();
   listenQueen(history);
 
   return (
     <>
-      {queenSwState && (
-        <div className={classes.queenApp}>
-          <queen-app />
-        </div>
-      )}
+      {queenSwState && <queen-app />}
       {!queenSwState && <h2>Queen service-worker not available.</h2>}
     </>
   );
