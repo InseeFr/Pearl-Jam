1e5535c6fca1f9e474dd67b819418493725d9ce4
Dylan Decrulle <dylan.decrulle@outlook.fr>
2023-03-27 19:39:38 +0200

    migration jest to vitest

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+    coverage: {
+      reporter: ['text', 'html'],
+      exclude: ['node_modules/', 'src/setupTests.js'],
+    },
+  },
   build: { outDir: 'build' },
   plugins: [tsconfigPaths(), react()],
 });
