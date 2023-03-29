e4129d3d7c96252826c04959dc7c8aa2648078ab
Dylan Decrulle <dylan.decrulle@outlook.fr>
2023-03-29 13:55:21 +0200

    make build esnext

--- vite.config.js
+++ vite.config.js
@@ -32,7 +32,7 @@ export default defineConfig({
       exclude: ['node_modules/', 'src/setupTests.js'],
     },
   },
-  build: { outDir: 'build' },
+  build: { outDir: 'build', target: 'esnext' },
   plugins: [
     tsconfigPaths(),
     react(),
