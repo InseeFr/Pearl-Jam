bfe34481cc8d727eef7097fb65d0c2561eaac591
Simon Demazière <simon.demaziere@insee.fr>
2023-07-11 16:02:18 +0200

    feat: check address validity before sending comm

--- src/components/panel-body/UEpage/communication/form/CommunicationRequestForm.jsx
+++ src/components/panel-body/UEpage/communication/form/CommunicationRequestForm.jsx
@@ -28,95 +28,104 @@ export const CommunicationRequestForm = ({
   userInformation,
   recipientInformation,
 }) => {
-  const classes = useStyles();
+        const classes = useStyles();
 
-  const [communicationRequest, setCommunicationRequest] = useState(EMPTY_COMMUNICATION_REQUEST);
-  const steps = COMMUNICATION_REQUEST_FORM_STEPS;
-  const [selectedStep, setSelectedStep] = useState(steps[0]);
+        const [communicationRequest, setCommunicationRequest] = useState(
+          EMPTY_COMMUNICATION_REQUEST
+        );
+        const steps = COMMUNICATION_REQUEST_FORM_STEPS;
+        const [selectedStep, setSelectedStep] = useState(steps[0]);
 
-  const [currentAttributeIsValid, setCurrentAttributeIsValid] = useState(false);
+        const [currentAttributeIsValid, setCurrentAttributeIsValid] = useState(false);
 
-  const getStepIndex = () => steps.findIndex(step => step.title === selectedStep.title);
-  const isFirstStep = () => getStepIndex() === 0;
-  const isLastStep = () => getStepIndex() === steps.length - 1;
+        const getStepIndex = () => steps.findIndex(step => step.title === selectedStep.title);
+        const isFirstStep = () => getStepIndex() === 0;
+        const isLastStep = () => getStepIndex() === steps.length - 1;
 
-  const generateCommunicationRequest = commRequ => ({
-    ...commRequ,
-    status: [{ date: new Date().getTime(), status: communicationStatusEnum.INITIATED.type }],
-  });
+        const generateCommunicationRequest = commRequ => ({
+          ...commRequ,
+          status: [{ date: new Date().getTime(), status: communicationStatusEnum.INITIATED.type }],
+        });
 
-  const nextStepAction = () => {
-    if (isLastStep()) {
-      saveFunction(generateCommunicationRequest(communicationRequest));
-      closeModalFunction();
-    } else {
-      setSelectedStep(steps[getStepIndex() + 1]);
-    }
-  };
+        const nextStepAction = () => {
+          if (isLastStep()) {
+            saveFunction(generateCommunicationRequest(communicationRequest));
+            closeModalFunction();
+          } else {
+            setSelectedStep(steps[getStepIndex() + 1]);
+          }
+        };
 
-  const previousStepAction = () => {
-    if (isFirstStep()) {
-      closeModalFunction();
-    } else {
-      setSelectedStep(steps[getStepIndex() - 1]);
-    }
-  };
+        const previousStepAction = () => {
+          if (isFirstStep()) {
+            closeModalFunction();
+          } else {
+            setSelectedStep(steps[getStepIndex() - 1]);
+          }
+        };
 
-  const isValueValid = value => !!value;
+        const isValueValid = value => !!value;
 
-  const isAddressingValid = () => {
-    // TODO : add business rules here
-    return true;
-  };
+        const isAddressingValid = () => {
+          // TODO : add business rules here
+          return true;
+        };
 
-  const checkValidity = useCallback((communicationRequest, selectedStep) => {
-    if (selectedStep.valueName !== undefined)
-      return isValueValid(communicationRequest[selectedStep.valueName]);
+        const checkValidity = useCallback((communicationRequest, selectedStep) => {
+          if (selectedStep.valueName !== undefined)
+            return isValueValid(communicationRequest[selectedStep.valueName]);
 
-    return isAddressingValid();
-  }, []);
+          return isAddressingValid();
+        }, []);
 
-  useEffect(() => {
-    setCurrentAttributeIsValid(checkValidity(communicationRequest, selectedStep));
-  }, [checkValidity, communicationRequest, selectedStep]);
+        useEffect(() => {
+          setCurrentAttributeIsValid(checkValidity(communicationRequest, selectedStep));
+        }, [checkValidity, communicationRequest, selectedStep]);
 
-  const handleChange = event => {
-    const {
-      target: { value },
-    } = event;
-    setCommunicationRequest({ ...communicationRequest, [selectedStep.valueName]: value });
-  };
-  const addressesErrors = checkCommunicationRequestFormAddressesValidity(
-    recipientInformation,
-    userInformation,
-    communicationRequest
-  );
+        const handleChange = event => {
+          const {
+            target: { value },
+          } = event;
+          setCommunicationRequest({ ...communicationRequest, [selectedStep.valueName]: value });
+        };
+        const addressesErrors = checkCommunicationRequestFormAddressesValidity(
+          recipientInformation,
+          userInformation,
+          communicationRequest
+        );
 
-  return (
-    <>
-      <Typography className={classes.title}>{selectedStep.title}</Typography>
-      {selectedStep.valueName ? (
-        <FormRadioGroup
-          groupSelectedValue={communicationRequest[selectedStep.valueName]}
-          groupHandleChange={handleChange}
-          groupValues={selectedStep.values}
-        />
-      ) : (
-        <CommunicationRequestValidation
-          communicationRequest={communicationRequest}
-          recipientInformation={recipientInformation}
-          userInformation={userInformation}
-          addressesErrors={addressesErrors}
-        />
-      )}
-      <div className={classes.buttons}>
-        <Button variant="outlined" onClick={previousStepAction}>
-          {selectedStep.previousLabel}
-        </Button>
-        <Button disabled={!currentAttributeIsValid} onClick={nextStepAction}>
-          {selectedStep.nextLabel}
-        </Button>
-      </div>
-    </>
-  );
-};
+        const addressesHaveError = Object.values(addressesErrors)
+          .map(Object.values)
+          .flat()
+          .some(error => error === true);
+
+        const buttonIsDisabled = selectedStep.valueName ? !currentAttributeIsValid : addressesHaveError;
+
+        return (
+          <>
+            <Typography className={classes.title}>{selectedStep.title}</Typography>
+            {selectedStep.valueName ? (
+              <FormRadioGroup
+                groupSelectedValue={communicationRequest[selectedStep.valueName]}
+                groupHandleChange={handleChange}
+                groupValues={selectedStep.values}
+              />
+            ) : (
+              <CommunicationRequestValidation
+                communicationRequest={communicationRequest}
+                recipientInformation={recipientInformation}
+                userInformation={userInformation}
+                addressesErrors={addressesErrors}
+              />
+            )}
+            <div className={classes.buttons}>
+              <Button variant="outlined" onClick={previousStepAction}>
+                {selectedStep.previousLabel}
+              </Button>
+              <Button disabled={buttonIsDisabled} onClick={nextStepAction}>
+                {selectedStep.nextLabel}
+              </Button>
+            </div>
+          </>
+        );
+      };
