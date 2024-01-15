4e90704022a1ea9acc2ddf2364c1294787f8f58f
Menet Mickaël <mickaelmenet@gmail.com>
2024-01-15 10:27:24 +0100

    add icons

---
+++ src/ui/Questionnaire/Icons/EditIcon.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { makeStyles } from '@mui/styles';
+import EditNoteOutlinedIcon from '@mui/icons-material/EditNoteOutlined';
+
+
+const useStyles = makeStyles({
+  
+  icon: {
+    marginLeft: '16px'
+  },
+});
+
+export default function EditIcon() {
+  const classes = useStyles();
+  return (
+    <EditNoteOutlinedIcon className={classes.icon} />
+  )
+}
