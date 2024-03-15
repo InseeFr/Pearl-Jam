cee614893090a162bae15072468c4a6d5ed35ade
Simon Demaziere <simon.demaziere@insee.fr>
2024-03-15 13:56:15 +0100

    feat: i18n

--- src/i18n/trackingMessage.js
+++ src/i18n/trackingMessage.js
@@ -1,11 +1,11 @@
 const trackingMessage = {
   trackingAccessDetailedData: {
     fr: 'Pour accéder au détail, cliquez directement sur l’enquête souhaitée.',
-    en: 'Mister',
+    en: 'To access the details, click on a campaign',
   },
   trackingSelectCampaign: {
     fr: 'Sélectionnez une enquête dans la liste déroulante ci-dessus.',
-    en: 'Miss',
+    en: 'Select a campaign from the drop-down list above.',
   },
   trackingLastContactAttempt: { fr: 'Dernier essai de contact', en: 'Last contact attempt' },
   trackingSurveyUnitStatus: { fr: "Status de l'unité", en: 'Survey-unit status' },
