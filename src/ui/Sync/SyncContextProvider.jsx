dfcf55c2c19273d35b04438ee752cfbdccda2412
Menet Mickaël <mickaelmenet@gmail.com>
2024-03-18 14:34:16 +0100

    add forgotten useMemo

--- src/ui/Sync/SyncContextProvider.jsx
+++ src/ui/Sync/SyncContextProvider.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import * as api from 'utils/api';
 
 import { analyseResult, getNotifFromResult, saveSyncPearlData } from 'utils/synchronize/check';
