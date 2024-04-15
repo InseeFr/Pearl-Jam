73fca3fed509acc659cf63e4b0bb4d4c41876b42
Menet Mickaël <mickaelmenet@gmail.com>
2024-04-15 08:32:10 +0200

    add circleIcon test

---
+++ src/ui/Questionnaire/Icons/CircleIcon.test.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import CircleIcon from './CircleIcon';
+
+describe('CircleIcon Component', () => {
+  it('renders without errors', () => {
+    render(<CircleIcon />);
+  });
+});
