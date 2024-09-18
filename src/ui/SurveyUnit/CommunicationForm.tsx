1c0449b9d27dacb7fa5c8791987d1dde8f9c0c0c
Pierre-Raphaël Wozny <pierre-raphael.wozny@insee.fr>
2024-09-18 13:42:00 +0200

    feat: retrieving mediums, types and reasons from possible radio values and computing disabled values from selected form's choice/communications templates data

--- src/ui/SurveyUnit/CommunicationForm.tsx
+++ src/ui/SurveyUnit/CommunicationForm.tsx
@@ -14,6 +14,35 @@ namespace Steps {
     return value - 1;
   }
 }
+  let mediums = mediumRadioValues.map(m => {
+    return {
+      value: m.value,
+      label: m.label,
+      disabled: !communicationTemplates.some(c => c.medium.value === m.value),
+    };
+  });
+
+  communicationTemplates = communicationTemplates.filter(
+    item => item.medium.value === communicationRequest.medium
+  );
+
+  let types = typeRadioValues.map(t => {
+    return {
+      value: t.value,
+      label: t.label,
+      disabled: !communicationTemplates.some(c => c.type.value === t.value),
+    };
+  });
+
+  let reasons = reasonRadioValues.map(r => {
+    return {
+      value: r.value,
+      label: r.label,
+      disabled: !communicationTemplates.some(
+        c => c.type.value === communicationTypeEnum.COMMUNICATION_REMINDER.value
+      ),
+    };
+  });
   const nextStep = () => {
     if (step < Steps.VALIDATE) setStep(Steps.after(step));
   };
