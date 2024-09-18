c868f485105375de381e8558278e2e48dec01b79
Pierre-Raphaël Wozny <pierre-raphael.wozny@insee.fr>
2024-09-18 14:20:26 +0200

    feat: updated CommunicationForm.tsx hooks and props

--- src/ui/SurveyUnit/CommunicationForm.tsx
+++ src/ui/SurveyUnit/CommunicationForm.tsx
@@ -31,6 +31,53 @@ namespace Steps {
     return value - 1;
   }
 }
+
+export interface CommunicationForm {
+  medium: string;
+  reason: string;
+  type: string;
+}
+
+interface CommunicationFormProps {
+  onClose: () => void;
+  surveyUnit: SurveyUnit;
+}
+
+/**
+ * Form to add a new contact attempt to a survey unit
+ *
+ * @param {() => void} onClose
+ * @param {SurveyUnit} surveyUnit
+ * @returns {TSX.Element}
+ */
+export function CommunicationForm({ onClose, surveyUnit }: CommunicationFormProps) {
+  const [step, setStep] = useState(Steps.MEDIUM);
+  const [comRequestValidity, setComRequestValidity] = useState(false);
+  const [communicationRequest, setCommunicationRequest] = useState<CommunicationForm>({
+    medium: '',
+    reason: '',
+    type: '',
+  });
+
+  // const communicationTemplates = surveyUnit?.communicationTemplates ?? [];
+  let communicationTemplates = [
+    {
+      medium: communicationMediumEnum.MEDIUM_EMAIL,
+      type: communicationTypeEnum.COMMUNICATION_NOTICE,
+      id: 1,
+    },
+    {
+      medium: communicationMediumEnum.MEDIUM_MAIL,
+      type: communicationTypeEnum.COMMUNICATION_REMINDER,
+      id: 2,
+    },
+    {
+      medium: communicationMediumEnum.MEDIUM_EMAIL,
+      type: communicationTypeEnum.COMMUNICATION_NOTICE,
+      id: 3,
+    },
+  ];
+
   let mediums = mediumRadioValues.map(m => {
     return {
       value: m.value,
@@ -60,6 +107,7 @@ namespace Steps {
       ),
     };
   });
+
   // If reminder is not selected as a type, the user does not have to set a reason (therefore setting it automatically here)
   if (
     communicationRequest.type !== communicationTypeEnum.COMMUNICATION_REMINDER.value &&
@@ -70,6 +118,39 @@ namespace Steps {
       reason: communicationReasonEnum.UNREACHABLE.value,
     });
 
+  const saveCommunicationRequest = () => {
+    // Retrieveing communicationTemplatedId by using form's input from the user
+    const communicationTemplatedId = communicationTemplates.find(
+      communicationTemplate =>
+        communicationTemplate.type.value === communicationRequest.type &&
+        communicationTemplate.medium.value === communicationRequest.medium
+    )?.id;
+
+    const newCommunicationRequest = {
+      reason: communicationRequest.reason,
+      emitter: 'INTERVIEWER',
+      communicationTemplateId: communicationTemplatedId,
+      status: [{ date: new Date().getTime(), status: communicationStatusEnum.INITIATED.type }],
+    } as SurveyUnitCommunicationRequest;
+
+    surveyUnitIDBService.addOrUpdateSU({
+      ...surveyUnit,
+      communicationRequests: [...(surveyUnit.communicationRequests ?? []), newCommunicationRequest],
+    });
+
+    onClose();
+    setCommunicationRequest({ medium: '', reason: '', type: '' });
+  };
+
+  const setValue = (value: string, prop: string) => {
+    setCommunicationRequest({
+      ...communicationRequest,
+      [prop]: value,
+    });
+  };
+
+  const isFirst = step === Steps.MEDIUM;
+
   const nextStep = () => {
     if (step < Steps.VALIDATE) setStep(Steps.after(step));
   };
@@ -79,6 +160,19 @@ namespace Steps {
       setStep(Steps.before(step));
     }
   };
+
+  // if (
+  //   step === Steps.REASON &&
+  //   communicationRequest.type != communicationTypeEnum.COMMUNICATION_REMINDER.value
+  // ) {
+  //   setCommunicationRequest({
+  //     ...communicationRequest,
+  //     reason: communicationReasonEnum.UNREACHABLE.value,
+  //   });
+  //   setUnreachableNotice(true);
+  //   nextStep();
+  // }
+
   return (
     <Dialog maxWidth="sm" open={true} onClose={onClose()}>
       {step == Steps.MEDIUM && (
