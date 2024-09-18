f3acd58009140313c5efe9816c082a4cb35342aa
Pierre-Raphaël Wozny <pierre-raphael.wozny@insee.fr>
2024-09-18 14:29:15 +0200

    feat: added CommunicationDialogContent.tsx, used for each form field (medium, type, reason) in CommunicationForm.tsx

--- src/ui/SurveyUnit/Communication/CommunicationDialogContent.tsx
+++ src/ui/SurveyUnit/Communication/CommunicationDialogContent.tsx
@@ -1,3 +1,60 @@
 import { DialogTitle, DialogContent, Box, RadioGroup, Stack } from '@mui/material';
 import { RadioLine } from '../../RadioLine';
 import { useEffect, useState } from 'react';
+
+interface CommunicationDialogContentProps {
+  step: string;
+  options: {
+    label: string;
+    value: string;
+    disabled : boolean
+  }[];
+  setCommunicationValue: Function;
+  title: string;
+  lastPickedPropValue: string;
+}
+
+const CommunicationDialogContent = ({
+  step,
+  options,
+  setCommunicationValue,
+  title,
+  lastPickedPropValue,
+}: CommunicationDialogContentProps) => {
+  console.log(options);
+  console.log(options.find(o => !o.disabled)?.value);
+
+    
+  const [value, setValue] = useState(
+    options.find(o => o.value === lastPickedPropValue) ? lastPickedPropValue : (options.find(o => !o.disabled)?.value) ?? []
+  );
+
+  useEffect(() => {
+    setCommunicationValue(value, step);
+  }, [value]);
+
+
+  return (
+    <>
+      <DialogTitle id="dialogtitle">{title}</DialogTitle>
+      <DialogContent>
+        <Box>
+          <RadioGroup
+            onChange={e => setValue(e.target.value)}
+            row
+            aria-labelledby="dialogtitle"
+            name="communication-radio-group"
+            value={value}
+          >
+            <Stack gap={1} width={1}>
+              {options.map(o => (
+                <RadioLine value={o.value} label={o.label} disabled={o.disabled} />
+              ))}
+            </Stack>
+          </RadioGroup>
+        </Box>
+      </DialogContent>
+    </>
+  );
+};
+export default CommunicationDialogContent;
