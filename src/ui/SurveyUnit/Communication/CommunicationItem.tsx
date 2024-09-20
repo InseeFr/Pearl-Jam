f81056c9c7c0984c72be89afa6472b4f463ecaaf
Pierre-Raphaël Wozny <pierre-raphael.wozny@insee.fr>
2024-09-20 10:53:10 +0200

    fix : formats/imports update

--- src/ui/SurveyUnit/Communication/CommunicationItem.tsx
+++ src/ui/SurveyUnit/Communication/CommunicationItem.tsx
@@ -7,25 +7,26 @@ import D from 'i18n';
 import { formatDate } from '../../../utils/functions/date';
 import { HEALTHY_COMMUNICATION_REQUEST_STATUS } from '../../../utils/constants';
 import {
-    findCommunicationMediumLabelByValue,
-    findCommunicationReasonLabelByValue,
-    findCommunicationStatusValueByType,
-    findCommunicationTypeLabelByValue,
+  findCommunicationMediumLabelByValue,
+  findCommunicationReasonLabelByValue,
+  findCommunicationStatusValueByType,
+  findCommunicationTypeLabelByValue,
 } from '../../../utils/enum/CommunicationEnums';
 import { Row } from '../../Row';
 import { Typography } from '../../Typography';
 
-
-interface CommunicationItemProps
-{
-  communication : SurveyUnitCommunicationRequest
-  surveyUnitCommunicationTemplate : SurveyUnitCommunicationTemplate | undefined
+interface CommunicationItemProps {
+  communication: SurveyUnitCommunicationRequest;
+  surveyUnitCommunicationTemplate: SurveyUnitCommunicationTemplate | undefined;
 }
 /**
  * @param {SurveyUnitCommunicationRequest} communication
  * @constructor
  */
-export function CommunicationItem({ communication, surveyUnitCommunicationTemplate } : CommunicationItemProps) {
+export function CommunicationItem({
+  communication,
+  surveyUnitCommunicationTemplate,
+}: CommunicationItemProps) {
   const IconComponent =
     communication.emitter.toUpperCase() === 'INTERVIEWER' ? DirectionsWalkIcon : BuildIcon;
   const sortedStatus = [...communication.status].sort((s1, s2) => s1.date - s2.date);
