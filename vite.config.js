8866538b737cb2fc0383f9659272c2712ed42ee3
Pierre-Raphaël Wozny <pierre-raphael.wozny@insee.fr>
2024-09-20 14:40:56 +0200

    fix: revert

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,6 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000
-  },
   test: {
     globals: true,
     environment: 'jsdom',
