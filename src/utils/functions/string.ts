3d815386884cadca2c91b3325337aa386696c509
Emmanuel <demey.emmanuel@gmail.com>
2024-11-08 10:34:54 +0000

    feat: delete unused file and migrate string.js

--- src/utils/functions/string.ts
+++ src/utils/functions/string.ts
@@ -1,12 +1,10 @@
 /**
  * Normalizes a string converting special character.
  *
- * @param {string} string
- * @returns {string}
  * @example
  * normalize("Café") // cafe;
  */
-export function normalize(string) {
+export function normalize(string: string) {
   return string
     .normalize('NFD')
     .replace(/[\u0300-\u036f]/g, '')
