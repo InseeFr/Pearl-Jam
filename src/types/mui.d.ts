459f79d6add13112023fe36053d17684518df85c
Emmanuel <demey.emmanuel@gmail.com>
2024-11-26 10:53:59 +0000

    feat: migrate Preloader component

--- src/types/mui.d.ts
+++ src/types/mui.d.ts
@@ -1,6 +1,15 @@
+import { CircularProgressProps } from '@mui/material/CircularProgress';
+//import { BoxProps } from '@mui/material/Box';
+
 // Update the Card's variant prop options
 declare module '@mui/material/Card' {
   interface CardPropsVariantOverrides {
     disabled: true;
+  }
+}
+
+declare module '@mui/material/CircularProgress' {
+  interface CircularProgressPropsColorOverrides {
+    white: true;
   }
 }
