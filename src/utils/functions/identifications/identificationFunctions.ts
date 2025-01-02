c023d20cf22b9a112dd713d90d7b5c3e52c0ec16
Pierre-Raphaël Wozny <pierre-raphael.wozny@insee.fr>
2025-01-02 15:33:39 +0100

    fix: fixed switch case

--- src/utils/functions/identifications/identificationFunctions.ts
+++ src/utils/functions/identifications/identificationFunctions.ts
@@ -135,7 +135,9 @@ export const checkValidityForTransmissionIasco = (su: SurveyUnit) => {
 export const isValidForTransmission = (su: SurveyUnit) => {
   const { identificationConfiguration } = su;
   switch (identificationConfiguration) {
-    case IdentificationConfiguration.IASCO || IdentificationConfiguration.HOUSEF2F:
+    case IdentificationConfiguration.IASCO:
+      return checkValidityForTransmissionIasco(su);
+    case IdentificationConfiguration.HOUSEF2F:
       return checkValidityForTransmissionIasco(su);
     case IdentificationConfiguration.INDTEL:
       return validateTransmissionArray(transmissionRulesByTel, su);
