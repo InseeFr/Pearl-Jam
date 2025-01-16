2ac099faf071a5c05d7ad7611ccd494c17189929
Pierre-Raphaël Wozny <pierre-raphael.wozny@insee.fr>
2025-01-16 13:54:08 +0100

    fix: safe StatusChip status access

--- src/ui/StatusChip.tsx
+++ src/ui/StatusChip.tsx
@@ -1,9 +1,10 @@
 import { ToDoEnumValues } from '../utils/enum/SUToDoEnum';
 import Chip from '@mui/material/Chip';
 import { useTheme } from '@mui/material/styles';
+import D from 'i18n';
 
 interface StatusChipProps {
-  status: ToDoEnumValues;
+  status?: ToDoEnumValues;
 }
 
 export function StatusChip({ status }: Readonly<StatusChipProps>) {
@@ -11,12 +12,12 @@ export function StatusChip({ status }: Readonly<StatusChipProps>) {
   return (
     <Chip
       sx={{
-        backgroundColor: status.color,
+        backgroundColor: status?.color ?? theme.palette.primary.light,
         height: 28,
         color: theme.palette.primary.main,
         fontWeight: '600',
       }}
-      label={status.value}
+      label={status?.value ?? D.missingLabel}
     />
   );
 }
