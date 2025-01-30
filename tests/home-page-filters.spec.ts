6e06e3936d28007ee6442689456bab7b58e573d7
Pierre-Raphaël Wozny <pierre-raphael.wozny@insee.fr>
2025-01-30 14:00:34 +0100

    fix: updated playwirght tests

--- tests/home-page-filters.spec.ts
+++ tests/home-page-filters.spec.ts
@@ -6,19 +6,19 @@ test('check if filters are saved inside the localStorage', async ({ page }) => {
   await homePage.go();
   await homePage.importData();
 
-  await homePage.checkNumberOfDisplayedItems(16, 16);
+  await homePage.checkNumberOfDisplayedItems(17, 17);
   await page.getByLabel('A préparer').check();
   await page.getByLabel('secondtestcampaign').check();
-  await homePage.checkNumberOfDisplayedItems(1, 16);
+  await homePage.checkNumberOfDisplayedItems(1, 17);
 
   await page.reload();
 
-  await homePage.checkNumberOfDisplayedItems(1, 16);
+  await homePage.checkNumberOfDisplayedItems(1, 17);
 
   await page.getByRole('checkbox', { name: 'A préparer', checked: true }).uncheck();
   await page.getByRole('checkbox', { name: 'secondtestcampaign', checked: true }).uncheck();
 
-  await homePage.checkNumberOfDisplayedItems(16, 16);
+  await homePage.checkNumberOfDisplayedItems(17, 17);
 });
 
 test('check if the status filters is working properly', async ({ page }) => {
@@ -28,43 +28,43 @@ test('check if the status filters is working properly', async ({ page }) => {
 
   await page.getByLabel('A préparer').check();
 
-  await homePage.checkNumberOfDisplayedItems(1, 16);
+  await homePage.checkNumberOfDisplayedItems(1, 17);
 
   await homePage.resetAllFilters();
 
   await page.getByLabel('A repérer/Contacter').check();
 
-  await homePage.checkNumberOfDisplayedItems(8, 16);
+  await homePage.checkNumberOfDisplayedItems(8, 17);
 
   await homePage.resetAllFilters();
 
   await page.getByLabel('A enquêter').check();
 
-  await homePage.checkNumberOfDisplayedItems(6, 16);
+  await homePage.checkNumberOfDisplayedItems(6, 17);
 
   await homePage.resetAllFilters();
 
   await page.getByLabel('A finaliser').check();
 
-  await homePage.checkNumberOfDisplayedItems(0, 16);
+  await homePage.checkNumberOfDisplayedItems(0, 17);
 
   await homePage.resetAllFilters();
 
   await page.getByLabel('A transmettre').check();
 
-  await homePage.checkNumberOfDisplayedItems(1, 16);
+  await homePage.checkNumberOfDisplayedItems(1, 17);
 
   await homePage.resetAllFilters();
 
   await page.getByLabel('A synchroniser').check();
 
-  await homePage.checkNumberOfDisplayedItems(0, 16);
+  await homePage.checkNumberOfDisplayedItems(0, 17);
 
   await homePage.resetAllFilters();
 
   await page.getByLabel('Terminé', { exact: true }).check();
 
-  await homePage.checkNumberOfDisplayedItems(0, 16);
+  await homePage.checkNumberOfDisplayedItems(0, 17);
 
   await homePage.resetAllFilters();
 });
@@ -73,9 +73,9 @@ test('check if the priority filter is working properly', async ({ page }) => {
   const homePage = new HomePage(page);
   await homePage.go();
   await homePage.importData();
-  await homePage.checkNumberOfDisplayedItems(16, 16);
+  await homePage.checkNumberOfDisplayedItems(17, 17);
   await page.getByLabel('Unités prioritaires').check();
-  await homePage.checkNumberOfDisplayedItems(5, 16);
+  await homePage.checkNumberOfDisplayedItems(5, 17);
   await homePage.resetAllFilters();
 });
 
@@ -83,13 +83,13 @@ test('check if the Survey filter is working properly', async ({ page }) => {
   const homePage = new HomePage(page);
   await homePage.go();
   await homePage.importData();
-  await homePage.checkNumberOfDisplayedItems(16, 16);
+  await homePage.checkNumberOfDisplayedItems(17, 17);
   await page.getByLabel('secondtestcampaign').check();
-  await homePage.checkNumberOfDisplayedItems(11, 16);
+  await homePage.checkNumberOfDisplayedItems(11, 17);
   await homePage.resetAllFilters();
-  await homePage.checkNumberOfDisplayedItems(16, 16);
+  await homePage.checkNumberOfDisplayedItems(17, 17);
   await page.getByLabel('testcampaign', { exact: true }).check();
-  await homePage.checkNumberOfDisplayedItems(5, 16);
+  await homePage.checkNumberOfDisplayedItems(5, 17);
   await homePage.resetAllFilters();
 });
 
@@ -99,11 +99,11 @@ test('check if the cluster filter is working properly', async ({ page }) => {
   await homePage.importData();
   await page.getByRole('combobox').first().click();
   await page.getByRole('option', { name: '2' }).click();
-  await homePage.checkNumberOfDisplayedItems(11, 16);
+  await homePage.checkNumberOfDisplayedItems(11, 17);
   await page.getByRole('combobox').nth(1).click();
   await page.getByRole('option', { name: '1' }).click();
-  await page.getByText('unités sur 16').click();
-  await homePage.checkNumberOfDisplayedItems(0, 16);
+  await page.getByText('unités sur 17').click();
+  await homePage.checkNumberOfDisplayedItems(0, 17);
 });
 
 test('check if the reset feature is working properly', async ({ page }) => {
@@ -120,13 +120,13 @@ test('check if the reset feature is working properly', async ({ page }) => {
   expect(filter1).toBeChecked();
   expect(filter2).toBeChecked();
 
-  await homePage.checkNumberOfDisplayedItems(1, 16);
+  await homePage.checkNumberOfDisplayedItems(1, 17);
 
   await homePage.resetAllFilters();
 
   expect(filter1).not.toBeChecked();
   expect(filter2).not.toBeChecked();
-  await homePage.checkNumberOfDisplayedItems(16, 16);
+  await homePage.checkNumberOfDisplayedItems(17, 17);
 });
 
 test('check if the search input is working properly', async ({ page }) => {
@@ -134,7 +134,7 @@ test('check if the search input is working properly', async ({ page }) => {
   await homePage.go();
   await homePage.importData();
   await page.getByPlaceholder('Nom, prénom, ville, enquête,').fill('graham');
-  await homePage.checkNumberOfDisplayedItems(7, 16);
+  await homePage.checkNumberOfDisplayedItems(7, 17);
 });
 
 test('check if the order input is working properly', async ({ page }) => {
