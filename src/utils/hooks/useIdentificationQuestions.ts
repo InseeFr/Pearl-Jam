2379e566942fb201888b31589b53eb9e9dfb7a3b
Pierre-Raphaël Wozny <pierre-raphael.wozny@insee.fr>
2025-02-04 18:30:26 +0100

    fix: removed unused import

--- src/utils/hooks/useIdentificationQuestions.ts
+++ src/utils/hooks/useIdentificationQuestions.ts
@@ -9,7 +9,6 @@ import {
   IdentificationQuestionOption,
   IdentificationQuestions,
   getIdentificationQuestionsTree,
-  IdentificationQuestionValue,
   ResponseState,
 } from 'utils/functions/identifications/identificationFunctions';
 
