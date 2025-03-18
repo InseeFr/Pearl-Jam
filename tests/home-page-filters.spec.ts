004f5700ae593aa3a1a71206be4e14f1bc0c90bf
Emmanuel <demey.emmanuel@gmail.com>
2025-03-18 10:21:31 +0100

    playwright with docker

--- tests/home-page-filters.spec.ts
+++ tests/home-page-filters.spec.ts
@@ -16,7 +16,7 @@ test('check if filters are saved inside the localStorage', async ({ page }) => {
   await homePage.checkNumberOfDisplayedItems(1);
 
   await page.getByRole('checkbox', { name: 'A préparer', checked: true }).uncheck();
-  await page.getByRole('checkbox', { name: 'secondtestcampaign', checked: true }).uncheck();
+  await page.getByRole('checkbox', { name: 'vqs2021x00', checked: true }).uncheck();
 
   await homePage.checkNumberOfDisplayedItems(totalSu);
 });
@@ -58,13 +58,21 @@ test('check if the status filters is working properly', async ({ page }) => {
 
   await page.getByLabel('A synchroniser').check();
 
+<<<<<<< HEAD
   await homePage.checkNumberOfDisplayedItems(0);
+=======
+  await homePage.checkNumberOfDisplayedItems(0, 25);
+>>>>>>> 30f23e1 (playwright with docker)
 
   await homePage.resetAllFilters();
 
   await page.getByLabel('Terminé', { exact: true }).check();
 
+<<<<<<< HEAD
   await homePage.checkNumberOfDisplayedItems(0);
+=======
+  await homePage.checkNumberOfDisplayedItems(0, 25);
+>>>>>>> 30f23e1 (playwright with docker)
 
   await homePage.resetAllFilters();
 });
@@ -73,9 +81,15 @@ test('check if the priority filter is working properly', async ({ page }) => {
   const homePage = new HomePage(page);
   await homePage.go();
   await homePage.importData();
+<<<<<<< HEAD
   await homePage.checkNumberOfDisplayedItems(totalSu);
   await page.getByLabel('Unités prioritaires').check();
   await homePage.checkNumberOfDisplayedItems(5);
+=======
+  await homePage.checkNumberOfDisplayedItems(14, 25);
+  await page.getByLabel('Unités prioritaires').check();
+  await homePage.checkNumberOfDisplayedItems(3, 25);
+>>>>>>> 30f23e1 (playwright with docker)
   await homePage.resetAllFilters();
 });
 
@@ -83,6 +97,7 @@ test('check if the Survey filter is working properly', async ({ page }) => {
   const homePage = new HomePage(page);
   await homePage.go();
   await homePage.importData();
+<<<<<<< HEAD
   await homePage.checkNumberOfDisplayedItems(totalSu);
   await page.getByLabel('secondtestcampaign').check();
   await homePage.checkNumberOfDisplayedItems(13);
@@ -90,6 +105,15 @@ test('check if the Survey filter is working properly', async ({ page }) => {
   await homePage.checkNumberOfDisplayedItems(totalSu);
   await page.getByLabel('testcampaign', { exact: true }).check();
   await homePage.checkNumberOfDisplayedItems(5);
+=======
+  await homePage.checkNumberOfDisplayedItems(14, 25);
+  await page.getByLabel('vqs2021x00').check();
+  await homePage.checkNumberOfDisplayedItems(3, 25);
+  await homePage.resetAllFilters();
+  await homePage.checkNumberOfDisplayedItems(14, 25);
+  await page.getByLabel('aqv2023x00', { exact: true }).check();
+  await homePage.checkNumberOfDisplayedItems(5, 25);
+>>>>>>> 30f23e1 (playwright with docker)
   await homePage.resetAllFilters();
 });
 
@@ -98,12 +122,20 @@ test('check if the cluster filter is working properly', async ({ page }) => {
   await homePage.go();
   await homePage.importData();
   await page.getByRole('combobox').first().click();
+<<<<<<< HEAD
   await page.getByRole('option', { name: '2' }).click();
   await homePage.checkNumberOfDisplayedItems(13);
   await page.getByRole('combobox').nth(1).click();
   await page.getByRole('option', { name: '1' }).click();
   await page.getByText(`unités sur ${totalSu}`).click();
   await homePage.checkNumberOfDisplayedItems(0);
+=======
+  await page.getByRole('option', { name: '1' }).click();
+  await homePage.checkNumberOfDisplayedItems(11, 25);
+  await page.getByRole('combobox').nth(1).click();
+  await page.getByRole('option', { name: '15' }).click();
+  await homePage.checkNumberOfDisplayedItems(1, 25);
+>>>>>>> 30f23e1 (playwright with docker)
 });
 
 test('check if the reset feature is working properly', async ({ page }) => {
@@ -112,29 +144,42 @@ test('check if the reset feature is working properly', async ({ page }) => {
   await homePage.importData();
 
   await page.getByLabel('A préparer').check();
-  await page.getByLabel('secondtestcampaign').check();
+  await page.getByLabel('vqs2021x00').check();
 
   const filter1 = await page.getByLabel('A préparer');
-  const filter2 = await page.getByLabel('secondtestcampaign');
+  const filter2 = await page.getByLabel('vqs2021x00');
 
   expect(filter1).toBeChecked();
   expect(filter2).toBeChecked();
 
+<<<<<<< HEAD
   await homePage.checkNumberOfDisplayedItems(1);
+=======
+  await homePage.checkNumberOfDisplayedItems(3, 25);
+>>>>>>> 30f23e1 (playwright with docker)
 
   await homePage.resetAllFilters();
 
   expect(filter1).not.toBeChecked();
   expect(filter2).not.toBeChecked();
+<<<<<<< HEAD
   await homePage.checkNumberOfDisplayedItems(totalSu);
+=======
+  await homePage.checkNumberOfDisplayedItems(14, 25);
+>>>>>>> 30f23e1 (playwright with docker)
 });
 
 test('check if the search input is working properly', async ({ page }) => {
   const homePage = new HomePage(page);
   await homePage.go();
   await homePage.importData();
+<<<<<<< HEAD
   await page.getByPlaceholder('Nom, prénom, ville, enquête,').fill('graham');
   await homePage.checkNumberOfDisplayedItems(9);
+=======
+  await page.getByPlaceholder('Nom, prénom, ville, enquête,').fill('lamoth');
+  await homePage.checkNumberOfDisplayedItems(1, 25);
+>>>>>>> 30f23e1 (playwright with docker)
 });
 
 test('check if the order input is working properly', async ({ page }) => {
@@ -145,5 +190,5 @@ test('check if the order input is working properly', async ({ page }) => {
   await page.getByRole('option', { name: 'Priorité' }).click();
   await page.getByLabel('delete').click();
   const first = await page.$('.MuiCardContent-root');
-  expect(await first?.innerText()).toContain('DUBUQUE-2 Clementina-2');
+  expect(await first?.innerText()).toContain('MOREAU Isabelle');
 });
--- tests/page-object/home.po.ts
+++ tests/page-object/home.po.ts
@@ -6,14 +6,24 @@ export const totalSu = 18;
 export class HomePage implements GenericPage {
   constructor(private readonly page: Page) {}
 
-  go() {
-    return this.page.goto('/', { timeout: 10000 });
+  async go() {
+    await this.page.goto('/', { timeout: 10000 });
+    await this.page.getByRole('textbox', { name: 'Username or email' }).click();
+    await this.page.getByRole('textbox', { name: 'Username or email' }).fill('interv5');
+    await this.page.getByRole('textbox', { name: 'Username or email' }).press('Tab');
+    await this.page.getByRole('textbox', { name: 'Password' }).fill('interv5');
+    await this.page.getByRole('button', { name: 'Sign In' }).click();
+    await expect(
+      this.page.getByText(
+        "L'application est prête à être utilisée hors ligne. (Pensez à synchroniser vos données avant)"
+      )
+    ).toBeVisible();
+    await this.page.getByRole('button', { name: 'Fermer' }).click();
   }
 
   async importData() {
-    await this.page.getByLabel('DevTools').hover();
-    await this.page.getByRole('button', { name: 'Importer des données de test' }).click();
-    await this.page.getByText('Mon suivi').hover();
+    await this.page.getByRole('button', { name: 'Synchroniser' }).click();
+    await this.page.getByRole('button', { name: "J'ai compris" }).click();
   }
 
   async checkNumberOfDisplayedItems(count: number) {
