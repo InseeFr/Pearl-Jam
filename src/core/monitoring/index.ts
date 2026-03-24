2a595a129a4dace9fe2b7fbd3ecf50b9113cadfe
Laurent Caouissin <laurent.caouissin@insee.fr>
2026-03-24 17:29:12 +0100

    chore: apm configuration

--- src/core/monitoring/index.ts
+++ src/core/monitoring/index.ts
@@ -4,9 +4,19 @@ import { version as appVersion } from '../../../package.json';
 const monitoringEnabled = import.meta.env.VITE_ENABLE_MONITORING === 'true';
 const monitoringServer = import.meta.env.VITE_MONITORING_SERVER_URL;
 
+const getOrigin = (url: string) => {
+  const uri = new URL(url);
+  return uri.origin;
+};
+
 export const monitoringService = init({
   serviceName: 'sabiane-collecte-ui',
   serviceVersion: appVersion,
   serverUrl: monitoringServer,
   active: monitoringEnabled,
+  distributedTracingOrigins: [
+    import.meta.env.VITE_PEARL_API_URL,
+    import.meta.env.VITE_QUEEN_URL,
+    // we need QUEEN_API_URL, only known by Queen...
+  ].map(uri => getOrigin(uri)),
 });
