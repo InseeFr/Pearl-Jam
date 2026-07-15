60be6f512fd07a1bedcb048246a18902def1719a
farid.ait-karra <farid.ait-karra@insee.fr>
2026-07-15 07:47:16 +0200

    chore: add vite-envs.sh

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { viteEnvs } from 'vite-envs';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -38,13 +39,14 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'build',
+    outDir: 'dist',
     target: 'esnext',
     sourcemap: true,
   },
   plugins: [
     tsconfigPaths(),
     react(),
+    viteEnvs({ declarationFile: '.env' }),
     federation({
       name: 'Pearl',
       remotes: {
@@ -73,6 +75,9 @@ export default defineConfig({
         display: 'standalone',
         theme_color: '#000000',
         background_color: '#ffffff',
+      },
+      injectManifest: {
+        globPatterns: ['**\/*.{js,css,html,ico,webmanifest,webp,woff2}'],
       },
     }),
   ],
